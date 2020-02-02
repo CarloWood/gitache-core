090fed70eab6d1bc099632fe0dcb919b3ce4c446
Carlo Wood <carlo@alinoe.com>
2020-02-02 23:28:14 +0100

    WIP

--- main.cmake
+++ main.cmake
@@ -16,8 +16,8 @@ execute_process(COMMAND ${git_executable} rev-parse HEAD
   OUTPUT_VARIABLE head_sha1
   OUTPUT_STRIP_TRAILING_WHITESPACE
 )
-#set(GITACHE_CORE_SHA1 ${head_sha1})
 if (NOT head_sha1 STREQUAL GITACHE_CORE_SHA1)
+set(GITACHE_CORE_SHA1 ${head_sha1})
   # If the right SHA1 is not already checked out,
   message(DEBUG "head_sha1 = \"${head_sha1}\", GITACHE_CORE_SHA1 = \"${GITACHE_CORE_SHA1}\".")
   # check if the SHA1 is in the local repository.
