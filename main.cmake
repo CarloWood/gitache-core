c969f9d259d1209e6474982feffb1b25ee6b7956
Carlo Wood <carlo@alinoe.com>
2020-02-02 22:33:36 +0100

    WIP

--- main.cmake
+++ main.cmake
@@ -3,6 +3,7 @@
 
 set(GITACHE_CORE_SOURCE_DIR "${GITACHE_CORE_DIR}/source")
 list(APPEND CMAKE_PREFIX_PATH "${GITACHE_CORE_SOURCE_DIR}/hutils")
+message(STATUS "CMAKE_PREFIX_PATH = \"${CMAKE_PREFIX_PATH}\".")
 include(gitache_get_git_executable)
 
 gitache_get_git_executable(git_executable)
