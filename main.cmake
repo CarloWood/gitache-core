4884b2e826db3155d3cb1864eddcdc8bb68e5b64
Carlo Wood <carlo@alinoe.com>
2020-02-02 22:32:49 +0100

    WIP

--- main.cmake
+++ main.cmake
@@ -1,6 +1,9 @@
 # Our first task is to make sure that GITACHE_CORE_SHA1 is checked out.
 # This is done by executing git commands directly, so find git.
 
-include()
+set(GITACHE_CORE_SOURCE_DIR "${GITACHE_CORE_DIR}/source")
+list(APPEND CMAKE_PREFIX_PATH "${GITACHE_CORE_SOURCE_DIR}/hutils")
+include(gitache_get_git_executable)
 
-message(STATUS "We are here: ${CMAKE_CURRENT_LIST_FILE}; GITACHE_CORE_SHA1 = ${GITACHE_CORE_SHA1}")
+gitache_get_git_executable(git_executable)
+message(STATUS "git_executable = \"${git_executable}\".")
