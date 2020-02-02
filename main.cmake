128ba92de8e5693942d21e72fd6166930464792a
Carlo Wood <carlo@alinoe.com>
2020-02-02 23:27:31 +0100

    WIP

--- main.cmake
+++ main.cmake
@@ -9,14 +9,25 @@ gitache_get_git_executable(git_executable)
 message(DEBUG "git_executable = \"${git_executable}\".")
 
 # Check if the needed sha1 is in our local repository.
-execute_process(COMMAND git rev-parse HEAD
+
+# Get the SHA1 that is checkout out right now.
+execute_process(COMMAND ${git_executable} rev-parse HEAD
   WORKING_DIRECTORY ${GITACHE_CORE_SOURCE_DIR}
   OUTPUT_VARIABLE head_sha1
   OUTPUT_STRIP_TRAILING_WHITESPACE
 )
-set(GITACHE_CORE_SHA1 ${head_sha1})
+#set(GITACHE_CORE_SHA1 ${head_sha1})
 if (NOT head_sha1 STREQUAL GITACHE_CORE_SHA1)
-  message(STATUS "head_sha1 = \"${head_sha1}\", GITACHE_CORE_SHA1 = \"${GITACHE_CORE_SHA1}\".")
+  # If the right SHA1 is not already checked out,
+  message(DEBUG "head_sha1 = \"${head_sha1}\", GITACHE_CORE_SHA1 = \"${GITACHE_CORE_SHA1}\".")
+  # check if the SHA1 is in the local repository.
+  execute_process(COMMAND ${git_executable} cat-file -e "${GITACHE_CORE_SHA1}^{commit}"
+    WORKING_DIRECTORY ${GITACHE_CORE_SOURCE_DIR}
+    OUTPUT_QUIET
+    ERROR_QUIET
+    RESULT_VARIABLE _result_error
+  )
+  message(STATUS "_result_error = \"${_result_error}\".")
 else ()
   message(STATUS "Already equal!")
 endif ()
