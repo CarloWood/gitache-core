9d33b837baabcaaa55ca97079b81dcd195ff6cbb
Carlo Wood <carlo@alinoe.com>
2020-02-03 15:37:15 +0100

    WIP

--- main.cmake
+++ main.cmake
@@ -10,15 +10,26 @@ message(DEBUG "git_executable = \"${git_executable}\".")
 
 # Check if the needed sha1 is in our local repository.
 
-# Get the SHA1 that is checkout out right now.
+# Stop other processes from changing the SHA1.
+message(STATUS "Locking \"${GITACHE_CORE_SOURCE_DIR}\"...")
+file(LOCK ${GITACHE_CORE_SOURCE_DIR} DIRECTORY
+  GUARD PROCESS
+  RESULT_VARIABLE _error_result
+  TIMEOUT 10
+)
+if (_error_result)
+  message(FATAL_ERROR "  ${_error_result}.")
+endif ()
+
+# Get the SHA1 that is checked out right now.
 execute_process(COMMAND ${git_executable} rev-parse HEAD
   WORKING_DIRECTORY ${GITACHE_CORE_SOURCE_DIR}
   OUTPUT_VARIABLE head_sha1
   OUTPUT_STRIP_TRAILING_WHITESPACE
 )
+# If the right SHA1 is not already checked out,
 if (NOT head_sha1 STREQUAL GITACHE_CORE_SHA1)
   set(GITACHE_CORE_SHA1 "${head_sha1}")
-  # If the right SHA1 is not already checked out,
   message(DEBUG "head_sha1 = \"${head_sha1}\", GITACHE_CORE_SHA1 = \"${GITACHE_CORE_SHA1}\".")
   # check if the SHA1 is in the local repository.
   execute_process(COMMAND ${git_executable} cat-file -e "${GITACHE_CORE_SHA1}^{commit}"
@@ -26,10 +37,19 @@ if (NOT head_sha1 STREQUAL GITACHE_CORE_SHA1)
     ERROR_QUIET
     RESULT_VARIABLE _result_error
   )
-  if (_result_error EQUAL "0")
-    message(STATUS "SUCCESS: _result_error = \"${_result_error}\".")
-  else ()
-    message(STATUS "ERROR: _result_error = \"${_result_error}\".")
+  if (NOT _result_error EQUAL "0")
+    # That SHA1 is not known yet. Fetch it from upstream.
+    execute_process(COMMAND ${git_executable} fetch
+      WORKING_DIRECTORY ${GITACHE_CORE_SOURCE_DIR}
+    )
+  endif ()
+  # Now checkout the needed SHA1.
+  execute_process(COMMAND ${git_executable} checkout ${GITACHE_CORE_SHA1}
+    WORKING_DIRECTORY ${GITACHE_CORE_SOURCE_DIR}
+    RESULT_VARIABLE _result_error
+  )
+  if (_result_error)
+    message(FATAL_ERROR "Failed to checkout ${GITACHE_CORE_SHA1} of gitache-core!")
   endif ()
 else ()
   message(STATUS "Already equal!")
